7573f4076d4a586c9c3fa15190df1cbc3e8b2796
Atiya <matiyaaaa@gmail.com>
2024-05-13 00:01:38 +0400

    Update and rename server.lua to roll.lua

--- server/roll.lua
+++ server/roll.lua
@@ -1,34 +1,27 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function handleRollCommand(source, args, commandName)
-    local xPlayer = QBCore.Functions.GetPlayer(source)
-    
+    local Player = QBCore.Functions.GetPlayer(source)
     local maxNumber = tonumber(args[1]) or 100
     if maxNumber > Config.MaxRollNumber then
         TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'The maximum roll number is ' .. Config.MaxRollNumber .. '.' } })
         return
     end
-
     if commandName == Config.RollCommandName then
-        
         if not Config.RollEnabled then
             TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', Config.RollDisabledMessage } })
             return
         end
-
         if Config.RollEnabled then
             local rollResult = math.random(1, maxNumber)
             TriggerClientEvent('atiya-roll:diceRollResult', source, rollResult, maxNumber)
         end
-
     elseif commandName == Config.ShopRollCommandName then
-
         if not Config.ShopRollEnabled then
             TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', Config.ShopRollDisabledMessage } })
             return
         end
-
-        if Config.ShopRollAllAccess or Config.ShopRollAllowedJobs[xPlayer.PlayerData.job.name] then
+        if Config.ShopRollAllAccess or Config.ShopRollAllowedJobs[Player.PlayerData.job.name] then
             local winningNumber = tonumber(args[2])
             local rollResult = math.random(1, maxNumber)
             local isWinner = rollResult == winningNumber
@@ -37,6 +30,48 @@ local function handleRollCommand(source, args, commandName)
             TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', Config.ShopRollNoPermissionMessage } })
         end
     end
+end
+
+RegisterNetEvent('atiya-roll:diceRollResult')
+AddEventHandler('atiya-roll:diceRollResult', function(rollResult, maxNumber)
+    local src = source
+    local playerPed = GetPlayerPed(src)
+    local nearbyPlayers = GetPlayersWithinRadius(GetEntityCoords(playerPed), 50)
+
+    for _, playerID in ipairs(nearbyPlayers) do
+        TriggerClientEvent('atiya-roll:displayPlayer3DText', playerID, src, rollResult, maxNumber)
+    end
+end)
+
+RegisterNetEvent('atiya-roll:shopDiceRollResult')
+AddEventHandler('atiya-roll:shopDiceRollResult', function(rollResult, maxNumber, isWinner, winningEmojis)
+    local src = source
+    local playerPed = GetPlayerPed(src)
+    local coords = GetEntityCoords(playerPed)
+    local nearbyPlayers = GetPlayersWithinRadius(coords, 50)
+
+
+    for _, playerID in ipairs(nearbyPlayers) do
+        TriggerClientEvent('atiya-roll:startShuffle', playerID, src, maxNumber)
+    end
+    SetTimeout(Config.ShopRollShuffle.ShuffleDuration, function()
+        local finalText = isWinner and string.format("%s %d/%d %s", winningEmojis[1], rollResult, maxNumber, winningEmojis[2]) or string.format("%d/%d", rollResult, maxNumber)
+        for _, playerID in ipairs(nearbyPlayers) do
+            TriggerClientEvent('atiya-roll:displayPlayer3DText2', playerID, src, finalText, isWinner and Config.WinningColor or Config.RollColor, Config.TextDisplayDurationAfterShuffle)
+        end
+    end)
+end)
+
+function GetPlayersWithinRadius(coords, radius)
+    local players = GetPlayers()
+    local filteredPlayers = {}
+    for _, player in ipairs(players) do
+        local ped = GetPlayerPed(player)
+        if #(GetEntityCoords(ped) - coords) < radius then
+            table.insert(filteredPlayers, player)
+        end
+    end
+    return filteredPlayers
 end
 
 QBCore.Commands.Add(Config.RollCommandName, Config.Commands[Config.RollCommandName].description, Config.Commands[Config.RollCommandName].parameters, true, function(source, args)
